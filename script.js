c12a37cb624da385f85b9a20d23f3ea656a360a4
Liam-Coulson <coulsonl2209@gmail.com>
2023-06-09 16:32:04 +0100

    Add regex to handle number and operation inputs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const calculatorInputs = document.getElementById("calculatorInputs");
 const calculatorButtons = document.getElementsByClassName("calcBtn");
 const calculatorEquals = document.getElementById("calcEquals");
 const calculatorDel = document.getElementById("calcDel");
+const ERROR = "ERROR";
 
 class Calculator {
     constructor() {
@@ -23,6 +24,8 @@ class Calculator {
 };
 
 var calcDisplayQueue = [];
+//var operands = [];
+//var operators = [];
 
 // Events
 
@@ -34,7 +37,7 @@ Array.from(calculatorButtons).forEach(btn => {
 });
 
 calculatorEquals.addEventListener("click", function() {
-    evaluateDisplay(calcDisplayQueue);
+    operate(calcDisplayQueue);
     console.log(calcDisplayQueue);
 })
 
@@ -44,16 +47,86 @@ calculatorDel.addEventListener("click", function() {
 })
 
 //TODO
-function evaluateDisplay(calcDisplayQueue) {
+function operate(calcDisplayQueue) {
+    let previousInput = "";
+    let currentConcatenation = "";
+    let newCalcDisplayQueue = [];
+    let operatorList = [];
+    let operandList = [];
 
+    calcDisplayQueue.forEach(input => {
+        if (isNumberlike(input)) {
+            if (isNumberlike(previousInput) || previousInput == "") {
+                currentConcatenation += input;
+                previousInput = input;
+            }
+        } else {
+            if (currentConcatenation != "") {
+                newCalcDisplayQueue.push(currentConcatenation);
+                operandList.push(currentConcatenation);
+                currentConcatenation = "";
+            }
+            newCalcDisplayQueue.push(input);
+            operatorList.push(input);
+            previousInput = input;
+        }
+    })
+    
+    if (currentConcatenation != "") {
+        newCalcDisplayQueue.push(currentConcatenation);
+        operandList.push(currentConcatenation);
+    }
+
+    // Number inputs have now been pieced back together, e.g. ["2", ".", "5"] -> "2.5"
+    calcDisplayQueue = newCalcDisplayQueue;
+
+    operandList.forEach(number => {
+        // checks if the numbers are of the form "digits (decimal?) digits"
+        if(!isNumberlike(number)) {return ERROR}
+    })
+
+    while (operatorList.includes("÷")) {
+        opIndex = calcDisplayQueue.findIndex("÷");
+        preOpIndex = opIndex - 1;
+        prepreOpIndex = opIndex - 2;
+        postOpIndex = opIndex + 1;
+        postpostOpIndex = opIndex + 2;
+
+        if (preOpIndex < 0 || postOpIndex > calcDisplayQueue.length) {return ERROR}
+        if (prepreOpIndex < 0) {prepreOpIndex = -1}
+        if (postpostOpIndex > calcDisplayQueue.length) {postpostOpIndex = -1}
+        
+        if (isNumberlike(calcDisplayQueue[preOpIndex])) {
+            if (isNumberlike(calcDisplayQueue[postOpIndex])) {
+                // SUCCESS
+                Test.divide(parseInt(calcDisplayQueue[preOpIndex],
+                            parseInt(calcDisplayQueue[postOpIndex])))
+            }
+            else if (calcDisplayQueue[postOpIndex] == "-"
+            && postpostOpIndex != -1) {
+                if (isNumberlike(calcDisplayQueue[postpostOpIndex])) {
+                    // SUCCESS
+                    Test.divide(parseInt(calcDisplayQueue[preOpIndex]),
+                                parseInt("-"+calcDisplayQueue[postOpIndex]))
+                } else {return ERROR}
+            } else {return ERROR}
+        } else {return ERROR}
+    }
+
+}
+/**
+ * Returns true if the string passed is a number with or without a decimal point
+ * @param {string} num  A single character or longer number
+ * @returns {boolean}   
+ */
+function isNumberlike(num) {
+    if (length(num) == 1) {return (num >= "0" && num <= "9" || num == ".");}
+    else {return num.match(/^\\d*\\.?\\d*$/)}
 }
 
 var operand1 = 0
 var operand2 = 0
-var operation = 0
-
-const buttons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                ".", "+", "-", ""];
+var operation = ""
 
 var Test = new Calculator();
 
